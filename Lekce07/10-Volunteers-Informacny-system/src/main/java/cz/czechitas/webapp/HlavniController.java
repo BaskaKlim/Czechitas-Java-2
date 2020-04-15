9047c635d33ffd6da51e4bf5b930a087b67cef21
BaskaKlim <baska.klimekova@gmail.com>
2020-04-15 19:55:31 +0200

    simple html templates, using diferent form input types

---
+++ Lekce07/10-Volunteers-Informacny-system/src/main/java/cz/czechitas/webapp/HlavniController.java
@@ -0,0 +1,17 @@
+package cz.czechitas.webapp;
+
+import java.util.*;
+import java.util.concurrent.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.*;
+
+@Controller
+public class HlavniController {
+
+    @RequestMapping("/")
+    public ModelAndView zobrazSeznam() {
+       return new ModelAndView();
+    }
+
+}
