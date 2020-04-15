981589e854ede4c9647ffbf18feb6f95d5a04295
BaskaKlim <baska.klimekova@gmail.com>
2020-04-16 00:04:05 +0200

    updating MVC class about handler.addObjects and random logic

--- Lekce02/10-JavaDynamicWeb/src/main/java/cz/czechitas/webapp/HlavniController.java
+++ Lekce02/10-JavaDynamicWeb/src/main/java/cz/czechitas/webapp/HlavniController.java
@@ -11,6 +11,12 @@ public class HlavniController {
     @RequestMapping("/")
     public ModelAndView zobrazIndex() {
         ModelAndView drzakNaDataAjmenoSablony = new ModelAndView("index") ;
+        drzakNaDataAjmenoSablony.addObject("jmeno", "Barbara")   ; // natvrdo pridane meno,
+
+        Random generatorCisel= new Random();
+        int i = generatorCisel.nextInt(6)+1;
+
+        drzakNaDataAjmenoSablony.addObject("cisloNaKostke",i);
         return drzakNaDataAjmenoSablony;
     }
 
