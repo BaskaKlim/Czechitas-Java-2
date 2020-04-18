ce729e77c9df37b538184166cdc3520e05a2bf19
BaskaKlim <baska.klimekova@gmail.com>
2020-04-18 23:46:44 +0200

    add java class HipsterProperties with props for Object hipster from form

---
+++ Lekce05-Ukol/src/main/java/cz/czechitas/webapp/HipsterProperties.java
@@ -0,0 +1,71 @@
+
+package cz.czechitas.webapp;
+
+public class HipsterProperties {
+
+    private int age;
+    private int height;
+    private String haircolor;
+    private String hairlenght;
+    private String eyecolor;
+    private String beard;
+    private String style;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newValue) {
+        age = newValue;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int newValue) {
+        height = newValue;
+    }
+
+    public String getHaircolor() {
+        return haircolor;
+    }
+
+    public void setHaircolor(String newValue) {
+        haircolor = newValue;
+    }
+
+    public String getHairlenght() {
+        return hairlenght;
+    }
+
+    public void setHairlength(String newValue) {
+        hairlenght = newValue;
+    }
+
+    public String getEyecolor() {
+        return eyecolor;
+    }
+
+    public void setEyecolor(String newValue) {
+        eyecolor = newValue;
+    }
+
+    public String getBeard() {
+        return beard;
+    }
+
+    public void setBeard(String newValue) {
+        beard = newValue;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String newValue) {
+        style = newValue;
+    }
+}
+
+
