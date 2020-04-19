efb4e70afdc33cd05c022612c6780c9e42806982
BaskaKlim <baska.klimekova@gmail.com>
2020-04-19 23:32:34 +0200

    thymeleaf template for index.html

--- Lekce06/40-Seznam_Jmen/src/main/java/cz/czechitas/webapp/HlavniController.java
+++ Lekce06/40-Seznam_Jmen/src/main/java/cz/czechitas/webapp/HlavniController.java
@@ -9,33 +9,5 @@ import org.springframework.web.servlet.*;
 @Controller
 public class HlavniController {
 
-    private List<String> seznamJmen;
-
-    public HlavniController() {
-        seznamJmen = new CopyOnWriteArrayList<>();
-        seznamJmen.add("Albert Einstein");
-        seznamJmen.add("Nikola Tesla");
-        seznamJmen.add("Barbara Klimek");
-        seznamJmen.add("Eliska Milek");
+  
     }
-
-    @RequestMapping("/")
-    public ModelAndView zobrazIndex() {
-        ModelAndView modelAndViewHandler = new ModelAndView("inde");
-        modelAndViewHandler.addObject(
-                "seznamJmen", seznamJmen);
-        return modelAndViewHandler;
-    }
-
-    @RequestMapping(value = "/novy.html", method = RequestMethod.GET)
-    public ModelAndView zobraNovy() {
-        return new ModelAndView("novy");
-    }
-
-    @RequestMapping(value = "/novy.html", method = RequestMethod.POST)
-    public ModelAndView pracujNovy(@RequestParam String jmeno) {
-        seznamJmen.add(jmeno);
-        return new ModelAndView("redirect:/");
-    }
-
-}
