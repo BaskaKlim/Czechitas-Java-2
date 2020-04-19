c32116ad5ba707d8f65b1515590505af301d9681
BaskaKlim <baska.klimekova@gmail.com>
2020-04-19 23:24:43 +0200

    using while(true) cycklus for printing the names into console, is neverending cycle

--- Lekce06/Seznamy-new/src/main/java/cz/czechitas/SpousteciTrida.java
+++ Lekce06/Seznamy-new/src/main/java/cz/czechitas/SpousteciTrida.java
@@ -10,7 +10,18 @@ public class SpousteciTrida {
         seznamJmen.add("Nikola Tesla");
         seznamJmen.add("Jean-Paul Sartre");
 
-           
+
+        while (true) {
+            System.out.println("Seznam jmen: ");
+
+     /* for (String jmeno: seznamJmen){
+      System.out.println(jmeno);  }
+      */
+
+            for (int i = 0; i < seznamJmen.size(); i++) {
+                String jmeno = seznamJmen.get(i);
+                System.out.println(i + " " + jmeno);
+            }
 
         }
 
