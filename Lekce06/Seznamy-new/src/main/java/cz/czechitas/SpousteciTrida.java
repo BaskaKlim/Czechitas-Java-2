6d3f62fc8ca2a1959ebfb657792ad6c5e6d4f995
BaskaKlim <baska.klimekova@gmail.com>
2020-04-19 23:24:00 +0200

    adding List of Strings - arraylist of names

---
+++ Lekce06/Seznamy-new/src/main/java/cz/czechitas/SpousteciTrida.java
@@ -0,0 +1,19 @@
+package cz.czechitas;
+
+import java.util.*;
+
+public class SpousteciTrida {
+
+    public static void main(String[] args) {
+        List<String> seznamJmen = new ArrayList<>();
+        seznamJmen.add("Albert Eistein");
+        seznamJmen.add("Nikola Tesla");
+        seznamJmen.add("Jean-Paul Sartre");
+
+           
+
+        }
+
+
+    }
+}
