dde77446f44f2afa08722a0042f89cfe4dfda3fb
BaskaKlim <baska.klimekova@gmail.com>
2020-04-19 23:33:39 +0200

    adding List of CopyOnWriteArrayList and MVC for index.html to Controller

--- Lekce06/40-Seznam_Jmen/src/main/java/cz/czechitas/webapp/HlavniController.java
+++ Lekce06/40-Seznam_Jmen/src/main/java/cz/czechitas/webapp/HlavniController.java
@@ -8,6 +8,22 @@ import org.springframework.web.servlet.*;
 
 @Controller
 public class HlavniController {
+    private List<String> seznamJmen;
+
+    public HlavniController() {
+        seznamJmen = new CopyOnWriteArrayList<>();
+        seznamJmen.add("Albert Einstein");
+        seznamJmen.add("Nikola Tesla");
+        seznamJmen.add("Barbara Klimek");
+        seznamJmen.add("Eliska Milek");
+    }
+
+    @RequestMapping("/")
+    public ModelAndView zobrazIndex() {
+        ModelAndView modelAndViewHandler = new ModelAndView("inde");
+        modelAndViewHandler.addObject(
+                "seznamJmen", seznamJmen);
+        return modelAndViewHandler;
 
   
     }
