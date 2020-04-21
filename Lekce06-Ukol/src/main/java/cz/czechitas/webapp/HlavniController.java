070be3ae5db43ba17b8cd045e8e7480fc12fa026
BaskaKlim <baska.klimekova@gmail.com>
2020-04-21 13:25:37 +0200

    starting with simple architecture for website - homeless quiz

---
+++ Lekce06-Ukol/src/main/java/cz/czechitas/webapp/HlavniController.java
@@ -0,0 +1,25 @@
+package cz.czechitas.webapp;
+
+import java.io.*;
+import java.util.*;
+import org.springframework.core.io.*;
+import org.springframework.core.io.support.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.*;
+
+@Controller
+public class HlavniController {
+
+
+    @RequestMapping("/")
+
+    public ModelAndView zobrazIndex() {
+
+        return  new ModelAndView("inde");
+    }
+
+
+
+}
+
