b099d0a7fd1dd8f93cbb94a7353d95f0860bdb7d
BaskaKlim <baska.klimekova@gmail.com>
2020-04-21 13:54:06 +0200

    adding class HomelessFacesForm - holding properties for each faces from quiz

---
+++ Lekce06-Ukol/src/main/java/cz/czechitas/webapp/HomelessFacesForm.java
@@ -0,0 +1,16 @@
+package cz.czechitas.webapp;
+
+import java.util.*;
+
+public class HomelessFacesForm {
+    private List<Boolean> answer;
+
+    public List<Boolean> getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(List<Boolean> newValue) {
+        answer = newValue;
+    }
+
+}
