1de1fbf945f4457c8842736cdaa7f26d34c80095
BaskaKlim <baska.klimekova@gmail.com>
2020-04-21 23:44:39 +0200

    MVC handler for booklibrarz and requestMethod.GET

--- Lekce07-Ukol/src/main/java/cz/czechitas/webapp/HlavniController.java
+++ Lekce07-Ukol/src/main/java/cz/czechitas/webapp/HlavniController.java
@@ -15,7 +15,16 @@ public class HlavniController {
         return new ModelAndView("redirect:/booklibrary");
     }
 
-  
+
+    @RequestMapping(value = "/booklibrary", method = RequestMethod.GET)
+    public ModelAndView zobrazLibrary() {
+        ModelAndView modelAndViewhandler = new ModelAndView("inde");
+        //adding object to MVC articles, that i want to use in thymeleaf in html
+        modelAndViewhandler.addObject("library", library.showLibrary());
+        return modelAndViewhandler;
+    }
+
+
 
 
 }
