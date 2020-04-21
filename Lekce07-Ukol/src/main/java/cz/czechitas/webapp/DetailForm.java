cf523ad11c70bf9157686610eeb1d01c2fe3f003
BaskaKlim <baska.klimekova@gmail.com>
2020-04-21 23:50:05 +0200

    adding DetailForm class for handle all properties from detail form

---
+++ Lekce07-Ukol/src/main/java/cz/czechitas/webapp/DetailForm.java
@@ -0,0 +1,33 @@
+
+package cz.czechitas.webapp;
+
+public class DetailForm {
+
+    String author;
+    String title;
+    String detail;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String newValue) {
+        author = newValue;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String newValue) {
+        title = newValue;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String newValue) {
+        detail = newValue;
+    }
+}
