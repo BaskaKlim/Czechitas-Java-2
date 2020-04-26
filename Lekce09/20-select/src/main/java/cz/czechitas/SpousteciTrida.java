d5e279a4a27ee4a784235c794e56f6da3910609c
BaskaKlim <baska.klimekova@gmail.com>
2020-04-26 22:18:16 +0200

    MariaDbDataSource, JdbcTemplate and simple queryForObject communication with database

--- Lekce09/20-select/src/main/java/cz/czechitas/SpousteciTrida.java
+++ Lekce09/20-select/src/main/java/cz/czechitas/SpousteciTrida.java
@@ -1,9 +1,26 @@
 package cz.czechitas;
 
+import java.sql.*;
+import org.mariadb.jdbc.*;
+import org.springframework.jdbc.core.*;
+
 public class SpousteciTrida {
 
-    public static void main(String[] args) {
-        System.out.println("Zdravim, pozemstani! Zavedte me ke svemu vudci");
+    public static void main(String[] args) throws SQLException {
+        //instancia databazy
+        MariaDbDataSource configDatabase = new MariaDbDataSource();
+        //nastavenie databazy
+        configDatabase.setUrl("jdbc:mysql://localhost:3306/VideoBoss");
+        configDatabase.setUserName("student");
+        configDatabase.setUserName("password");
+        // pripojenie k databaze , argument konstruktoru je konfiguracia databazy
+        JdbcTemplate requestSender = new JdbcTemplate(configDatabase);
+
+        //TODO: zisti kolko je pocet zapisov = zakaznikov v databaze
+        
+       Long numberOfClients = requestSender.queryForObject("select count (*) from customers", Long.class);
+
+       System.out.println("V databaze je "+ numberOfClients + " zakaznikov");
     }
 
 }
