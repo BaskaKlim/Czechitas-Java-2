cb9022e750528c1143effdcf85f55cc292f8c10e
BaskaKlim <baska.klimekova@gmail.com>
2020-04-29 16:03:50 +0200

    add JDBC Template with parameter = configuration of our databaze

--- Lekce09-Ukol/JDBC-SeznamKontaktu/src/main/java/cz/czechitas/SpousteciTrida.java
+++ Lekce09-Ukol/JDBC-SeznamKontaktu/src/main/java/cz/czechitas/SpousteciTrida.java
@@ -2,6 +2,7 @@ package cz.czechitas;
 
 import java.sql.*;
 import org.mariadb.jdbc.*;
+import org.springframework.jdbc.core.*;
 
 public class SpousteciTrida {
 
@@ -12,7 +13,9 @@ public class SpousteciTrida {
         configDatabase.setUrl("jdbc:mysql://localhost:3306/VideoBoss");
         configDatabase.setUser("student");
         configDatabase.setPassword("password");
-        
+
+        // pripojenie k databaze , argument konstruktoru je konfiguracia databazy -JDBC je database API, rovnako ako hibernate
+        JdbcTemplate requestSender = new JdbcTemplate(configDatabase);
     }
 
 }
