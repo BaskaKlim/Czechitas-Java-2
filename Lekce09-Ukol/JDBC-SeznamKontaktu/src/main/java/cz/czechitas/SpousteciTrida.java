10268d113157014fd4bfd48a08c0bbedf071ed4a
BaskaKlim <baska.klimekova@gmail.com>
2020-04-29 16:00:35 +0200

    configuration of MariaDbDataSource

--- Lekce09-Ukol/JDBC-SeznamKontaktu/src/main/java/cz/czechitas/SpousteciTrida.java
+++ Lekce09-Ukol/JDBC-SeznamKontaktu/src/main/java/cz/czechitas/SpousteciTrida.java
@@ -1,9 +1,18 @@
 package cz.czechitas;
 
+import java.sql.*;
+import org.mariadb.jdbc.*;
+
 public class SpousteciTrida {
 
-    public static void main(String[] args) {
-        System.out.println("Zdravim, pozemstani! Zavedte me ke svemu vudci");
+    public static void main(String[] args) throws SQLException {
+        //instancia databazy
+        MariaDbDataSource configDatabase = new MariaDbDataSource();
+        //nastavenie databazy : url, user, passwort
+        configDatabase.setUrl("jdbc:mysql://localhost:3306/VideoBoss");
+        configDatabase.setUser("student");
+        configDatabase.setPassword("password");
+        
     }
 
 }
