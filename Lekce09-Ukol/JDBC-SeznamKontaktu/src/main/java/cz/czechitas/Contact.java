28b22954976029d65f5910220b60af52738b5d3c
BaskaKlim <baska.klimekova@gmail.com>
2020-04-29 16:31:39 +0200

    select one atribute of data from database, such as number or string.. = simple select

---
+++ Lekce09-Ukol/JDBC-SeznamKontaktu/src/main/java/cz/czechitas/Contact.java
@@ -0,0 +1,41 @@
+package cz.czechitas;
+
+public class Contact {
+
+    Long id;
+    String jmeno;
+    String telefonniCislo;
+    String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long newValue) {
+        id = newValue;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String newValue) {
+        jmeno = newValue;
+    }
+
+    public String getTelefonniCislo() {
+        return telefonniCislo;
+    }
+
+    public void setTelefonniCislo(String newValue) {
+        telefonniCislo = newValue;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String newValue) {
+        email = newValue;
+    }
+}
--- Lekce09-Ukol/JDBC-SeznamKontaktu/src/main/java/cz/czechitas/SpousteciTrida.java
+++ Lekce09-Ukol/JDBC-SeznamKontaktu/src/main/java/cz/czechitas/SpousteciTrida.java
@@ -10,12 +10,16 @@ public class SpousteciTrida {
         //instancia databazy
         MariaDbDataSource configDatabase = new MariaDbDataSource();
         //nastavenie databazy : url, user, passwort
-        configDatabase.setUrl("jdbc:mysql://localhost:3306/VideoBoss");
+        configDatabase.setUrl("jdbc:mysql://localhost:3306/SeznamKontaktu");
         configDatabase.setUser("student");
         configDatabase.setPassword("password");
 
         // pripojenie k databaze , argument konstruktoru je konfiguracia databazy -JDBC je database API, rovnako ako hibernate
         JdbcTemplate requestSender = new JdbcTemplate(configDatabase);
-    }
 
+        //TODO: select 1 atributu napr pocet vsetkych kontaktov, alebo meno konkretneho kontaktu
+
+        Long numberOfContact = requestSender.queryForObject("select count(*) from kontakt where id =1", Long.class);
+        System.out.println("pocet kontaktov je " + numberOfContact);
+    }
 }
