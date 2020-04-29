1b14ff1c7a61c18657f0f96675db82678e617918
BaskaKlim <baska.klimekova@gmail.com>
2020-04-29 21:06:28 +0200

    new project - console app for communication with database DailyPlanet

---
+++ Lekce09-Ukol/JDBC-DailyPlanet/src/main/java/cz/czechitas/SpousteciTrida.java
@@ -0,0 +1,13 @@
+package cz.czechitas;
+
+import java.sql.*;
+import java.util.*;
+import org.mariadb.jdbc.*;
+import org.springframework.dao.*;
+import org.springframework.jdbc.core.*;
+import org.springframework.jdbc.support.*;
+
+public class SpousteciTrida {
+
+
+}
