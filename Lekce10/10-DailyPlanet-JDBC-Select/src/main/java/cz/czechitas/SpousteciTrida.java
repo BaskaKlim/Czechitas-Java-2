dc332827ce2691d4c02e2bca7263053d4a89bb90
BaskaKlim <baska.klimekova@gmail.com>
2020-05-03 22:52:43 +0200

    config properties such as user, password and url

--- Lekce10/10-DailyPlanet-JDBC-Select/src/main/java/cz/czechitas/SpousteciTrida.java
+++ Lekce10/10-DailyPlanet-JDBC-Select/src/main/java/cz/czechitas/SpousteciTrida.java
@@ -6,6 +6,9 @@ public class SpousteciTrida {
 
     public static void main(String[] args) {
         MariaDbDataSource configDatabase = new MariaDbDataSource();
+        configDatabase.setUser("student");
+        configDatabase.setPassword("password");
+        configDatabase.setUrl("jdbc:mysql://localhost:3306");
     }
 
 }
