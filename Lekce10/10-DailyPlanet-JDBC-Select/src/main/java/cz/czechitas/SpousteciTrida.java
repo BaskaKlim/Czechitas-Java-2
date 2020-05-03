140dc9f3962fdf9631250b4161c59ff4dafe70e8
BaskaKlim <baska.klimekova@gmail.com>
2020-05-03 22:50:34 +0200

    MariaDb dependencies  and instance of MariaDbDataSource

---
+++ Lekce10/10-DailyPlanet-JDBC-Select/src/main/java/cz/czechitas/SpousteciTrida.java
@@ -0,0 +1,11 @@
+package cz.czechitas;
+
+import org.mariadb.jdbc.*;
+
+public class SpousteciTrida {
+
+    public static void main(String[] args) {
+        MariaDbDataSource configDatabase = new MariaDbDataSource();
+    }
+
+}
