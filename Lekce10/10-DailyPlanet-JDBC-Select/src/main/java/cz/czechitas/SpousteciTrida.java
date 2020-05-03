0675f495b1966ca715a22e8adccc5638a313a95c
BaskaKlim <baska.klimekova@gmail.com>
2020-05-03 22:55:36 +0200

    adding requestHandler

--- Lekce10/10-DailyPlanet-JDBC-Select/src/main/java/cz/czechitas/SpousteciTrida.java
+++ Lekce10/10-DailyPlanet-JDBC-Select/src/main/java/cz/czechitas/SpousteciTrida.java
@@ -1,14 +1,20 @@
 package cz.czechitas;
 
+import java.sql.*;
 import org.mariadb.jdbc.*;
+import org.springframework.jdbc.core.*;
 
 public class SpousteciTrida {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws SQLException {
         MariaDbDataSource configDatabase = new MariaDbDataSource();
         configDatabase.setUser("student");
         configDatabase.setPassword("password");
-        configDatabase.setUrl("jdbc:mysql://localhost:3306");
+        configDatabase.setUrl("jdbc:mysql://localhost:3306/VideoBoss");
+
+        JdbcTemplate requestHandler = new JdbcTemplate(configDatabase);
     }
+
+
 
 }
