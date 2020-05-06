c47fe6bd1f05c58d8ed92796ca093a5f56dc0d48
BaskaKlim <baska.klimekova@gmail.com>
2020-05-06 15:51:26 +0200

    adding MariaDbDataSource configuration, mapper and rewuestHandler into JdbcTemplateRepository

--- Lekce10-Ukol/20-Pexeso-Zadani/src/main/java/cz/czechitas/webapp/persistence/JdbcTemplatePexesoRepository.java
+++ Lekce10-Ukol/20-Pexeso-Zadani/src/main/java/cz/czechitas/webapp/persistence/JdbcTemplatePexesoRepository.java
@@ -1,7 +1,11 @@
 package cz.czechitas.webapp.persistence;
 
 
+import java.sql.*;
 import java.util.*;
+import org.mariadb.jdbc.*;
+import org.springframework.jdbc.core.*;
+import org.springframework.jdbc.datasource.lookup.*;
 import org.springframework.stereotype.*;
 import cz.czechitas.webapp.entity.*;
 
@@ -12,6 +16,21 @@ public class JdbcTemplatePexesoRepository implements PexesoRepository{
     RowMapper<HerniPlocha> mapper;
     JdbcTemplate requestHandler;
 
+    //construnctor with mariadb config
+    public JdbcTemplatePexesoRepository() {
+        try {
+            MariaDbDataSource configDatabase = new MariaDbDataSource();
+            configDatabase.setUser("student");
+            configDatabase.setPassword("password");
+            configDatabase.setUrl("jdbc:mysql://localhost:3306/DailyPlanet");
+
+            mapper = BeanPropertyRowMapper.newInstance(HerniPlocha.class);
+            requestHandler = new JdbcTemplate(configDatabase);
+
+        } catch (SQLException sqle) {
+            throw new DataSourceLookupFailureException("Chyba pripojeni do databaze");
+        }
+    }
 
     @Override
     public HerniPlocha findById(Long id) {
