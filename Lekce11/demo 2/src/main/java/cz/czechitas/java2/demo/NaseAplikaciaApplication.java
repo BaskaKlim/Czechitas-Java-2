ba397e469bf3aa16683d875c7d5af38918160d70
BaskaKlim <baska.klimekova@gmail.com>
2020-05-12 23:01:22 +0200

    adding materials for session 11 and 12

---
+++ Lekce11/demo 2/src/main/java/cz/czechitas/java2/demo/NaseAplikaciaApplication.java
@@ -0,0 +1,13 @@
+package cz.czechitas.java2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaseAplikaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaseAplikaciaApplication.class, args);
+	}
+
+}
---
+++ Lekce11/demo 2/src/test/java/cz/czechitas/java2/demo/NaseAplikaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.czechitas.java2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaseAplikaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
